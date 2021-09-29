c3c9bb0e85116e02938e11f632e0398332690a7e
bandgarmamta <bandgarmamta@gmail.com>
2021-09-29 11:17:11 +0530

    update css

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ function calculateProfitAndLoss(initial, quantity, current) {
     var loss = (initial - current) * quantity;
     var lossPercentage = (loss / initial) * 100;
     outputEl.textContent =
-      "You have lost " + loss + "Your loss perscentage is " + lossPercentage + "%";
+      "You have lost " + loss + " Your loss perscentage is " + lossPercentage + "%";
   } else if (current > initial) {
     var profit = (current - initial) * quantity;
     var profitPercentage = (profit / initial) * 100;
     outputEl.textContent =
-      "You have gained " + profit + "Your profit percentage is " + profitPercentage+"%" ;
+      "You have gained " + profit + " Your profit percentage is " + profitPercentage+"%" ;
   } else {
     outputEl.textContent = "No gain no pain";
   }
 }
 function checkProfitOrLoss() {
-  var initial = Number(initialPrice.value);
-  var quantity = Number(stocksQuantity.value);
-  var current = Number(currentPrice.value);
-  calculateProfitAndLoss(initial, quantity, current);
+  var ip = Number(initialPrice.value);
+  var qty = Number(stocksQuantity.value);
+  var cp = Number(currentPrice.value);
+  calculateProfitAndLoss(ip, qty, cp);
 }
 
 submitBtn.addEventListener("click", checkProfitOrLoss);
